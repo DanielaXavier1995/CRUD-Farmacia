b99b2e17e6b0b4fe6670ca35c2665490704cb442
Daniela Xavier <danielamxsoares@gmail.com>
2024-01-31 11:25:01 -0300

    feat: criar produtoRepository

--- src/main/java/com/generation/farmacia/repository/ProdutoRepository.java
+++ src/main/java/com/generation/farmacia/repository/ProdutoRepository.java
@@ -1,5 +1,11 @@
 package com.generation.farmacia.repository;
 
-public interface ProdutoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.generation.farmacia.model.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
 
 }
