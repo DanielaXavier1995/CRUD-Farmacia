54f6cfc4c5287041d5538a6ad86bdf93dffbb37c
Daniela Xavier <danielamxsoares@gmail.com>
2024-01-31 10:26:52 -0300

    feat: criar estrutura do repository

--- src/main/java/com/generation/farmacia/repository/CategoriaRepository.java
+++ src/main/java/com/generation/farmacia/repository/CategoriaRepository.java
@@ -1,5 +1,11 @@
 package com.generation.farmacia.repository;
 
-public interface CategoriaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.generation.farmacia.model.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
 
 }
